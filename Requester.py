77be54414c8ab47ddb2cca1b479666e3294e5f26
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2024-02-06 15:00:21 -0600

    Update Requester.py

--- Requester.py
+++ Requester.py
@@ -18,7 +18,6 @@ def get_magic_items():
 
 def filter_magic_items(magic_items, rarity=None, item_type=None, keyword=None):
     filtered_items = magic_items
-    print("filtered_items Before", filtered_items)
     if rarity:
         filtered_items = [item for item in filtered_items if item["rarity"] == rarity]
     if item_type:
@@ -41,7 +40,7 @@ def print_magic_items(magic_items):
         print("No magic items found.")
 
 
-def main():
+def magic_item_rec():
     magic_items = get_magic_items()
     if magic_items:
         rarity = input("Enter the rarity (common, uncommon, rare, very rare, legendary) or press Enter to skip: ").title()
@@ -56,6 +55,3 @@ def main():
         else:
             print("No filters provided. Showing all magic items:")
             print_magic_items(sort_magic_items(magic_items))
-
-
-
